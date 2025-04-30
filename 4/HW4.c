f2c01d01429491a68fe4bed8121b89d7dbfb09d0
acard5meg <alexandyrtcard@gmail.com>
2025-04-30 01:01:14 -0400

    finish min viable product, need to test

--- 4/HW4.c
+++ 4/HW4.c
@@ -25,9 +25,9 @@ int main(void) {
             case 1: add_address(&ip4_addr); num_addresses++; break;
             case 2: lookup_addr(ip4_addr); break;
             case 3: update_addr(ip4_addr); break;
-            // case 4: delete_address(&ip4_addr); break;
+            case 4: delete_address(&ip4_addr); break;
             case 5: display_list(ip4_addr); break;
-            // case 6: display_alias(ip4_addr); break;
+            case 6: display_alias(ip4_addr); break;
             case 7: save_list(ip4_addr); break;
             case 8: quit(); break;
             // NO DEFAULT CASE BECAUSE get_user_input() DOES ERROR CHECKING
--- 4/bst_functions.c
+++ 4/bst_functions.c
@@ -35,3 +35,66 @@ int size(struct address_t *node) {
         return 0;
     return size(node -> leftChild) + size(node -> rightChild) + 1;
 }
+
+struct address_t* delete_node(struct address_t *root, char key[])
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+
+    int comp_val = equal_name2(root -> alias, key);
+
+    // if (key < root->data)
+    if (comp_val > 0)
+    {
+        root->leftChild = delete_node(root->leftChild, key);
+    }
+    // else if (key > root->data)
+    else if (comp_val < 0)
+    {
+        root->rightChild = delete_node(root->rightChild, key);
+    }
+    else
+    {
+        if (root->leftChild == NULL)
+        {
+            struct address_t *temp = root->rightChild;
+            free(root);
+            return temp;
+        }
+        else if (root->rightChild == NULL)
+        {
+            struct address_t *temp = root->leftChild;
+            free(root);
+            return temp;
+        }
+        struct address_t *temp = find_smallest_node(root->rightChild);
+
+        char temp_str[75];
+
+        int i = 0;
+        for (; temp->alias[i] != '\0'; i++)
+            temp_str[i] = temp->alias[i];
+
+        temp_str[i] = '\0';
+
+        strcpy(root->alias, temp_str);
+
+        root->rightChild = delete_node(root->rightChild, temp->alias);
+    }
+    return root;
+ 
+}
+
+struct address_t* find_smallest_node(struct address_t *root)
+{
+    struct address_t *curr = root;
+
+    while (curr != NULL && curr->leftChild != NULL)
+    {
+        curr = curr->leftChild;
+    }
+    return curr;
+}
+
